0a5771c242aaaa29d8fc9873a8b7ead4144329ed
Gabby Peneyra <gabby@xpay.world>
2019-07-29 09:03:09 +0800

    Roles Controller

--- XPW.Admin/Controllers/RolesController.cs
+++ XPW.Admin/Controllers/RolesController.cs
@@ -1,15 +1,62 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
 using XPW.CommonData.XPWAdmin.DataContext;
 using XPW.CommonData.XPWAdmin.Entities;
 using XPW.Utilities.BaseContextManagement;
+using XPW.Utilities.Filtering;
+using XPW.Utilities.UtilityModels;
 
 namespace XPW.Admin.Controllers {
      [RoutePrefix("roles")]
      public class RolesController : BaseServiceController<Role, XPWAdminContext> {
-          [Route("test")]
-          [HttpGet]
-          public string Test() {
-               return "test";
+          [Route("save")]
+          [HttpPost]
+          [RequestFiltering]
+          public async Task<GenericResponseModel<Role>> Save(Role apiModel) {
+               return await Task.Run(() => {
+                    try {
+                         apiModel = Service.SaveReturn(apiModel);
+                    } catch (Exception ex) {
+                         ErrorMessage = ex.Message;
+                         ErrorDetails.Add(ex.Message);
+                    }
+                    return new GenericResponseModel<Role>() {
+                         Code                = string.IsNullOrEmpty(ErrorMessage) ? Utilities.Enums.CodeStatus.Success : Utilities.Enums.CodeStatus.Error,
+                         CodeStatus          = string.IsNullOrEmpty(ErrorMessage) ? Utilities.Enums.CodeStatus.Success.ToString() : Utilities.Enums.CodeStatus.Error.ToString(),
+                         ReferenceObject     = string.IsNullOrEmpty(ErrorMessage) ? apiModel : null,
+                         ErrorMessage        = string.IsNullOrEmpty(ErrorMessage) ? null : new ErrorMessage {
+                              Details        = ErrorDetails,
+                              ErrNumber      = "800.2",
+                              Message        = ErrorMessage
+                         }
+                    };
+               });
+          }
+          [Route("update/{id}")]
+          [HttpPut]
+          [RequestFiltering]
+          public async Task<GenericResponseModel<Role>> Update([FromUri]int id, [FromBody]Role apiModel) {
+               return await Task.Run(() => {
+                    try {
+                         var name = new XPWAdminContext().GetType().Name;
+                         apiModel.DateUpdated = DateTime.Now;
+                         apiModel = Service.UpdateReturn(apiModel);
+                    } catch (Exception ex) {
+                         ErrorMessage = ex.Message;
+                         ErrorDetails.Add(ex.Message);
+                    }
+                    return new GenericResponseModel<Role>() {
+                         Code = string.IsNullOrEmpty(ErrorMessage) ? Utilities.Enums.CodeStatus.Success : Utilities.Enums.CodeStatus.Error,
+                         CodeStatus = string.IsNullOrEmpty(ErrorMessage) ? Utilities.Enums.CodeStatus.Success.ToString() : Utilities.Enums.CodeStatus.Error.ToString(),
+                         ReferenceObject = string.IsNullOrEmpty(ErrorMessage) ? apiModel : null,
+                         ErrorMessage = string.IsNullOrEmpty(ErrorMessage) ? null : new ErrorMessage {
+                              Details = ErrorDetails,
+                              ErrNumber = "800.2",
+                              Message = ErrorMessage
+                         }
+                    };
+               });
           }
      }
 }
